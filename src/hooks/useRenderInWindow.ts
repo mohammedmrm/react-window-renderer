9bd6861f5399e4af8047693b6c898d4b24330535
Mohammed R. M <m.mohammed@geniodiligence.it>
2024-09-25 12:31:33 +0200

    refactoring

--- src/hooks/useRenderInWindow.ts
+++ src/hooks/useRenderInWindow.ts
@@ -1,30 +1,30 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from "react";
 
 const useRenderInWindow = () => {
-    const [open, setOpen] = useState(false)
-    const _window = useRef<Window | null>(null)
+  const [open, setOpen] = useState(false);
+  const _window = useRef<Window | null>(null);
 
-    useEffect(() => {
-        const handleWindowClose = () => {
-            setOpen(false)
-        }
+  useEffect(() => {
+    const handleWindowClose = () => {
+      setOpen(false);
+    };
 
-        const handleBeforeUnload = () => {
-            setOpen(false)
-            _window.current?.close()
-        }
+    const handleBeforeUnload = () => {
+      setOpen(false);
+      _window.current?.close();
+    };
+    const w = _window.current;
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    window.addEventListener("unload", handleWindowClose);
 
-        window.addEventListener('beforeunload', handleBeforeUnload)
-        window.addEventListener('unload', handleWindowClose)
+    return () => {
+      w?.close();
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      window.removeEventListener("unload", handleWindowClose);
+    };
+  }, []);
 
-        return () => {
-            _window.current?.close()
-            window.removeEventListener('beforeunload', handleBeforeUnload)
-            window.removeEventListener('unload', handleWindowClose)
-        }
-    }, [])
+  return { open, setOpen, _window };
+};
 
-    return { open, setOpen, _window }
-}
-
-export default useRenderInWindow
+export default useRenderInWindow;
