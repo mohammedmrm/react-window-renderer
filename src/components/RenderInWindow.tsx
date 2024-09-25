53f7a578f3e68a2bb413c5e440fc3aa8b0291551
Mohammed R. M <m.mohammed@geniodiligence.it>
2024-09-25 17:43:59 +0200

    update useEffect deps

--- src/components/RenderInWindow.tsx
+++ src/components/RenderInWindow.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from "react";
-import ReactDOM from "react-dom";
+import { createPortal } from "react-dom";
 import windowCloseIcon from "../assets/window-close.svg";
 import windowOpenIcon from "../assets/window-open.svg";
 import { IRenderInWindow } from "../types/index";
@@ -97,11 +97,11 @@ const RenderInWindow = ({
     } else {
       if (_window.current) {
         _window.current.close();
-        //returnWindow(null)
+        returnWindow(null);
       }
       setReady(false);
     }
-  }, [extraHeadHTMLTags, open, returnWindow, setOpen, windowConfig]);
+  }, [open]);
   React.useEffect(() => {
     // If open, create window and store in ref
     preparePopup();
@@ -111,7 +111,7 @@ const RenderInWindow = ({
   }, [open, preparePopup, returnWindow]);
 
   if (open && ready && _window.current) {
-    return ReactDOM.createPortal(
+    return createPortal(
       <div className="relative">
         {showCloseWindowIcon && (
           <span
